b53beb72e4ca12e19edd3e0c5976a4cf2353c292
Paulo Rogerio <paulo@intranet>
2023-05-11 08:12:11 -0300

    Acerto no sql

--- .docker/initdb/0001.sql
+++ .docker/initdb/0001.sql
@@ -1,6 +1,6 @@
 create table if not exists public.codigos_nfce(
     id serial not null primary key,
     codigo character varying not null,
-    status boolean default false,
+    processado boolean default false,
     create_at timestamp without time default now()
 );
